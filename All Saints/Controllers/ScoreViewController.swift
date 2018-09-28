f08aea192ebd54471fa6b479d9a65c2809f53ca8
Viktor Ten <viktor.ten@me.com>
2018-09-28 17:11:00 +0600

    Score Screen

---
+++ All Saints/Controllers/ScoreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScoreViewController.swift
+//  All Saints
+//
+//  Created by admin on 9/28/18.
+//  Copyright © 2018 DAR. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+    @IBAction func didTapDoneButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
